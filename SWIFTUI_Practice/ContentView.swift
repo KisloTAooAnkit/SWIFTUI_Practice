3619fdc7db3ca44cf6f8f94641c208118f8563d1
Ankit Singh <kislotaooankit@gmail.com>
2021-07-03 19:13:19 +0530

    Initial Commit

---
+++ SWIFTUI_Practice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SWIFTUI_Practice
+//
+//  Created by Ankit Singh on 03/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
